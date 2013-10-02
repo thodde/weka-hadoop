814232eb65bcdcca8bf3d361cabcae0c03266d19
thodde <trhodde@wpi.edu>
2013-10-02 07:40:14 +0200

    Changes to compile script

--- compile.sh
+++ compile.sh
@@ -5,5 +5,23 @@ javac -classpath ./:./lib/commons-logging-1.1.2.jar:./lib/weka.jar:./lib/hadoop-
 
 if [ $? -eq 0 ]
 then
-	echo "Compilation Successful."
+	echo "Compilation Successful"
 fi
+
+echo "Copying class files..."
+cp src/*.class src/output/
+
+echo "Building JAR file..."
+pushd src/
+jar -cvf weka-test.jar -C output/ .
+
+if [ $? -eq 0 ]
+then
+	echo "JAR created successfully"
+fi
+popd
+
+echo "Complete."
+
+# Run the JAR here? Just don't lost this command...
+hadoop jar weka-test.jar Run 10 weka.classifiers.trees.J48 ./slug.arff output/
