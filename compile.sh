37c854e69c4e1d44aff5d211cc1deeb543c7a2f9
Trevor Hodde <trhodde@wpi.edu>
2013-10-11 10:42:31 -0400

    Adding compile script

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Adding required JARs to classpath and compiling sources..."
+javac -classpath ./lib/commons-logging-1.1.2.jar:./lib/weka.jar:./lib/hadoop-1.2.1/hadoop-core-1.2.1.jar ./src/Run.java
